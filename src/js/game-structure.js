d90ae59e5dc076609bdd53043639807e673403c3
Michal Opalka <michalopalka998@gmail.com>
2019-05-27 22:05:49 +0200

    Modificated game structure

--- src/js/game-structure.js
+++ src/js/game-structure.js
@@ -1,11 +1,20 @@
-const gameSummary = {
+import * as DOM from './dom';
+
+export const gameSummary = {
    numbers: 0,
    wins: 0,
    losses: 0,
    draws: 0
 }
 
-const oneGame = {
+export const oneGame = {
    playerHand: '',
    aiHand: ''
+}
+
+export const countGames = () => {
+   DOM.pointsCounter.innerHTML = `<li>Games: ${gameSummary.numbers}</li>
+   <li>Wins: ${gameSummary.wins}</li>
+   <li>Losses: ${gameSummary.losses}</li>
+   <li>Draws: ${gameSummary.draws}</li>`
 }
