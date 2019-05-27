9a09ec2dd534bfdd5a8e2e285973737056c5f3f4
Michal Opalka <michalopalka998@gmail.com>
2019-05-27 21:37:45 +0200

    Created DOM library

---
+++ src/js/dom.js
@@ -0,0 +1,5 @@
+const playerOptions = document.querySelectorAll('#player-options img');
+const gameShowResult = document.querySelector('#game-show-result');
+const play = document.querySelector('#play-game');
+const pointsCounter = document.querySelector('#points-counter');
+const computerChose = document.querySelector('#ai-move');
